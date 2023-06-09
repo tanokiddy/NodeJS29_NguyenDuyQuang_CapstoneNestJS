8cce45546720531482ca985d6ea0f91bd8b0eeb1
Quang <duyquangvfu@gmail.com>
2023-06-09 17:04:58 +0700

    completed add new location

--- src/location/location.controller.ts
+++ src/location/location.controller.ts
@@ -1,16 +1,28 @@
 import { resModel } from './../model/resModel';
-import { Body, Controller, InternalServerErrorException, Post, Res } from '@nestjs/common';
+import { Body, Controller, InternalServerErrorException, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { LocationService } from './location.service';
 import { LocationDetail } from 'src/types/location';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 @Controller('location')
 export class LocationController {
   constructor(private readonly locationService: LocationService) {}
 
+  @UseInterceptors(FileInterceptor('imageUpload', {
+    storage: diskStorage({
+      destination: process.cwd() + "/public/img",
+      filename: (req, file, callback) => callback(null, Date.now() + "_" + file.originalname)
+    })
+  }))
   @Post('upload-location')
-  async uploadLocation(@Body() data: LocationDetail, @Res() res){
+  async uploadLocation(@Body() data: LocationDetail, @Res() res, @UploadedFile() file){
+    const newData = {
+      ...data,
+      image: file.filename
+    }
     try {
-      const location = await this.locationService.uploadLocation(data)
+      const location = await this.locationService.uploadLocation(newData)
       if(location){
         res.send(resModel.OK(location))
       } else {
